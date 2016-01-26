2c22bbce2fc6825d2d4a18ea97b0f2f54fac9516
Jennifer <fashed@gmail.com>
2016-01-26 17:39:18 -0500

    disable confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :omniauthable
+         :omniauthable
+         #,:confirmable
 
   validates :fullname, presence: true, length: {maximum: 50}
 
