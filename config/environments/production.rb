f7aabb17e839ea6d3e98d1135f1585b15637b96a
Jennifer <fashed@gmail.com>
2016-02-25 01:45:34 -0500

    Background images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://cityromancer.heroku.com' }
 
   config.action_mailer.perform_deliveries = true
+
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
